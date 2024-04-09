071e3e036d009a3ab654b46de39f9165148c4cf9
emmajohn3 <156856366+emmajohn3@users.noreply.github.com>
2024-04-09 17:46:41 -0400

    Add files via upload

---
+++ js/challenge3.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    // alert('loaded!');
+    let figcaptions = document.querySelectorAll('figcaption')
+    let figcaption2 = figcaptions[1];
+    alert(figcaption2.innerHTML)
+})
